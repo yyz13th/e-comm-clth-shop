83488cb36dbc58ac11b8d103e6e72ce5f105ba37
yyz13th <136233684+yyz13th@users.noreply.github.com>
2023-10-26 16:37:30 +0300

    moved to components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import './categories.styles.scss'
+import Directory from './components/directory/directory.component';
+
 
 const App = () => {
 
@@ -28,22 +29,9 @@ const App = () => {
       "title": "mens",
       "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
     }
-  ] 
+  ];
 
-  return (
-    <div className="categories-container">
-      {categories.map(({title, id, imageUrl}) => (
-        <div key={id} className="category-container">
-         <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}}/>
-        <div className="category-body-container">
-          <h2>{title}</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      ))}
-      
-    </div>
-  );
-}
+  return <Directory categories={categories} />;
+};
 
 export default App;
