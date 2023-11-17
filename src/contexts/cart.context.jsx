128b2766bad420791e70a23be5d33311b90ddbc5
yyz13th <136233684+yyz13th@users.noreply.github.com>
2023-11-17 15:16:52 +0300

    cart dropdown shows name/img/price/quant + icon counter is working

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
     // if has productToAdd is true (id's are the same)
@@ -23,6 +23,7 @@ export const CartContext = createContext({
     setIsCartOpen: () => {},
     cartItems: [], // basic state hence empty array
     addItemToCart: () => {},
+    cartCount: 0
 });
 
 
@@ -30,9 +31,17 @@ export const CartContext = createContext({
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
+    const [cartCount, setCartCount] = useState(0);
+
+    useEffect(() => {
+        const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
+        setCartCount(newCartCount);
+    }, [cartItems]);
+
+
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd));  
     }
-    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
+    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount };
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
