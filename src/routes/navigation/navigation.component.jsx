672b4f3c73e99f24f34f9cd552b5957bd58d2f75
yyz13th <136233684+yyz13th@users.noreply.github.com>
2023-11-27 14:39:16 +0300

    nav component is now is styled component in order to avoid style clashing

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -11,7 +11,11 @@ import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 
-import './navigation.styles.scss';
+// import './navigation.styles.scss';
+
+import { NavigationContainer, LogoContainer, NavLinksContainer, NavLink } from "./navigation.styles";
+
+
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
   const { isCartOpen } = useContext(CartContext);
@@ -23,27 +27,27 @@ const Navigation = () => {
   // console.log(currentUser);
     return(
       <Fragment>
-        <div className='navigation'>
-          <Link className='logo-container' to='/'>
+        <NavigationContainer>
+          <LogoContainer to='/'>
             <CrwnLogo  className='logo' />
-          </Link>
-          <div className='nav-links-container'>
-            <Link className='nav-link' to='/shop'>  
+          </LogoContainer>
+          <NavLinksContainer>
+            <NavLink to='/shop'>  
                 SHOP
-            </Link>
+            </NavLink>
             {
               currentUser ? (
-                <span className='nav-link' onClick={signOutUser}>SIGN OUT</span>
+                <NavLink as='span' onClick={signOutUser}>SIGN OUT</NavLink>
               ) : (
-                <Link className='nav-link' to='/auth'>  
+                <NavLink to='/auth'>  
                     SIGN IN
-                </Link>
+                </NavLink>
               )
             }
             <CartIcon />
-          </div>
+          </NavLinksContainer>
           {isCartOpen &&  <CartDropdown />}
-        </div>
+        </NavigationContainer>
         <Outlet/>
       </Fragment>
     );
