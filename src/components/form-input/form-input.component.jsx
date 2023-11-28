ebff036d46a71f88361ecb529706a6f3ab6865cb
yyz13th <136233684+yyz13th@users.noreply.github.com>
2023-11-28 17:06:12 +0300

    scss -> jsx styling

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,19 +1,20 @@
-import './form-input.styles.scss';
+// import './form-input.styles.scss';
+import { FormInputLabel, Input, Group } from "./form-input.styles";
 
 const FormInput = ({label, ...otherProps }) => {
     return (
 
         //if label exists => assign styles
-        <div className="group">
-            <input className="form-input" {...otherProps} />
+        <Group>
+            <Input {...otherProps} />
             {label && (
-                <label 
-                className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>
+                <FormInputLabel 
+                shrink={otherProps.value.length}>
                     {label}
-                </label>
+                </FormInputLabel>
             )}
 
-        </div>
+        </Group>
     )
 }
 
