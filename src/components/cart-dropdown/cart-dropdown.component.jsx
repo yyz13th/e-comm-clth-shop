349fac729671d66024767c5c2d000f5d92d341db
yyz13th <136233684+yyz13th@users.noreply.github.com>
2023-11-28 15:07:24 +0300

    changed to styled component for unique classnames

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,9 +3,12 @@ import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../contexts/cart.context';
 
 import Button from '../button/button.component';  
-import './cart-dropdown.styles.scss';
+
+// import './cart-dropdown.styles.scss';
+import {CartDropdownContainer, EmptyMessage, CartItems} from './cart-dropdown.styles';
 
 import CartItem from '../cart-item/cart-item.component';
+
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext); //imnport cartItems array whenever it is empty or not
@@ -18,14 +21,18 @@ const CartDropdown = () => {
     }
 
     return (
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'>
-                {cartItems.map((cartItem) => (
-                    <CartItem key={cartItem.id} cartItem={cartItem} />
-                ))}
-            </div>
+        <CartDropdownContainer>
+            <CartItems>
+                {
+                    cartItems.length ? (cartItems.map((cartItem) => (
+                        <CartItem key={cartItem.id} cartItem={cartItem} />
+                    ))) : (
+                        <EmptyMessage>Your cart is empty</EmptyMessage>
+                    )
+                }
+            </CartItems>
             <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-        </div>
+        </CartDropdownContainer>
     )
 
 };
