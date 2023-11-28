7af2d401ef9e2602a0ae4838be686a1ff380c527
yyz13th <136233684+yyz13th@users.noreply.github.com>
2023-11-28 16:47:24 +0300

    style component for icon container

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,8 +1,10 @@
 import { useContext } from 'react';
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+// import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import { CartContext } from '../../contexts/cart.context';
-import './cart-icon.styles.scss';
+// import './cart-icon.styles.scss';
+
+import {ShoppingIconContainer, CartIconContainer, ItemCount} from './cart-icon.styles.jsx';
 
 
 const CartIcon = () => {
@@ -11,10 +13,10 @@ const CartIcon = () => {
     const toogleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
     return (
-        <div className='cart-icon-container' onClick={toogleIsCartOpen}>
-            <ShoppingIcon className='shopping-icon'/>
-            <span className='item-count'>{cartCount}</span>
-        </div>
+        <CartIconContainer onClick={toogleIsCartOpen}>
+            <ShoppingIconContainer className='shopping-icon'/>
+            <ItemCount>{cartCount}</ItemCount>
+        </CartIconContainer>
     );
 };
 
