8387472d0bac50d5de77655347860cb2ef40f745
yyz13th <136233684+yyz13th@users.noreply.github.com>
2023-12-04 16:07:21 +0300

    redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,10 @@
 import {compose, createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 
-import rootReducer from './root-reducer';
+import {rootReducer} from './root-reducer';
+
+const middleWares = [logger];
+
+const composeEnhancers = compose(applyMiddleware(...middleWares)); //spread because number of middlewares could increase in future
+
+export const store = createStore(rootReducer, undefined, composeEnhancers);
